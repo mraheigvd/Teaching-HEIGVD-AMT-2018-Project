f819ca13ef5c5d61e48e232776227e90d62d9c54
kam_ <kamil.amrani@heig-vd.ch>
2018-10-05 14:54:53 +0200

    add more to model calsses

--- src/main/java/ch/heigvd/amt/wp1/model/App.java
+++ src/main/java/ch/heigvd/amt/wp1/model/App.java
@@ -1,5 +1,8 @@
 package ch.heigvd.amt.wp1.model;
 
 public class App {
-
+    private String name;
+    private String description;
+    private String apiKey;
+    private String apiSecret;
 }
--- src/main/java/ch/heigvd/amt/wp1/model/User.java
+++ src/main/java/ch/heigvd/amt/wp1/model/User.java
@@ -1,5 +1,7 @@
 package ch.heigvd.amt.wp1.model;
 
 public class User {
-
+    private String email;
+    private String password;
+    private String name;
 }
