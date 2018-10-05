43324f4ce8303569fb2b76b61909bbdfa4b5260f
kam_ <kamil.amrani@heig-vd.ch>
2018-10-05 14:26:50 +0200

    static field in DB

--- src/main/java/ch/heigvd/amt/wp1/business/DataBase.java
+++ src/main/java/ch/heigvd/amt/wp1/business/DataBase.java
@@ -7,20 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataBase {
-    private List<User> users;
-    private List<App> apps;
-
-    public DataBase() {
-        users = new ArrayList<User>();
-        apps = new ArrayList<App>();
-    }
+    private static List<User> users = new ArrayList<User>();
+    private static List<App> apps = new ArrayList<App>();
 
     public void registerNewUser(String email, String password) {
 
     }
 
     public void tryLoggin() {
-        
+
     }
 
     public void getUserProfile() {
