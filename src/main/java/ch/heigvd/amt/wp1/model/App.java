321dea60b58cc7ab21461ac415adb884585e031a
Mentor Reka <reka.mentor@gmail.com>
2018-10-07 14:40:15 +0200

    Moved all to data package

--- src/main/java/ch/heigvd/amt/wp1/model/App.java
+++
@@ -1,8 +0,0 @@
-package ch.heigvd.amt.wp1.model;
-
-public class App {
-    private String name;
-    private String description;
-    private String apiKey;
-    private String apiSecret;
-}
--- src/main/java/ch/heigvd/amt/wp1/model/User.java
+++
@@ -1,7 +0,0 @@
-package ch.heigvd.amt.wp1.model;
-
-public class User {
-    private String email;
-    private String password;
-    private String name;
-}
